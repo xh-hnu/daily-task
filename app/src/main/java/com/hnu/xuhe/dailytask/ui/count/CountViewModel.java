072b3111e86fbe76a366436c33929df0df8e1a2b
xuhe <xuhe@hnu.edu.cn>
2019-09-12 16:21:00 +0800

    修改CountFragment

--- app/src/main/java/com/hnu/xuhe/dailytask/ui/count/CountViewModel.java
+++ app/src/main/java/com/hnu/xuhe/dailytask/ui/count/CountViewModel.java
@@ -10,7 +10,7 @@ public class CountViewModel extends ViewModel {
 
     public CountViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is Count fragment");
     }
 
     public LiveData<String> getText() {
