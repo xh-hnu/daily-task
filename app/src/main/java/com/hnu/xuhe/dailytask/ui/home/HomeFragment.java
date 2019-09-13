4656061b65878897fa55b2d6d98542e3f720ad3b
xh-hnu <xuhe@hnu.edu.cn>
2019-09-13 10:46:33 +0800

    小修改

--- app/src/main/java/com/hnu/xuhe/dailytask/ui/home/HomeFragment.java
+++ app/src/main/java/com/hnu/xuhe/dailytask/ui/home/HomeFragment.java
@@ -1,23 +1,19 @@
 package com.hnu.xuhe.dailytask.ui.home;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.hnu.xuhe.dailytask.R;
 import com.hnu.xuhe.dailytask.ui.home.entity.TaskItem;
-import com.hnu.xuhe.dailytask.ui.home.entity.adapter.MyAdapter;
+import com.hnu.xuhe.dailytask.ui.home.adapter.MyAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
