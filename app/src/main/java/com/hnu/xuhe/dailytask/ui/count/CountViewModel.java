35719c79ed35f841874a392f60483679bb4e15ab
xh-hnu <xuhe@hnu.edu.cn>
2019-09-15 15:18:18 +0800

    小修改

--- app/src/main/java/com/hnu/xuhe/dailytask/ui/count/CountViewModel.java
+++ app/src/main/java/com/hnu/xuhe/dailytask/ui/count/CountViewModel.java
@@ -1,7 +1,5 @@
 package com.hnu.xuhe.dailytask.ui.count;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class CountViewModel extends ViewModel {
